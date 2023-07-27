2a93ccd94dda0ae9b32573996c0e86908924de84
jxnipat <Jetnipat.prc@gmail.com>
2023-07-27 14:30:32 +0700

    upppppp lap5

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -38,6 +38,36 @@
 int main() {
 
     //--| YOUR CODE HERE
+    float Score ;
+    printf("enter score :\n");
+    scanf("%f" , &Score);
+    if (Score >= 80) {
+        printf("A !");
+    }
+    else if (Score >=75 && Score <80) {
+        printf("B+ !");
+    }
+    else if (Score >=70 && Score <75) {
+        printf("B !");
+    }
+    else if (Score >=65 && Score <70) {
+        printf("C+ !");
+    }
+    else if (Score >=60 && Score <65) {
+        printf("C !");
+    }
+    else if (Score >=55 && Score <60) {
+        printf("D+ !");
+    }
+    else if (Score >=50 && Score <55) {
+        printf("D !");
+    }
+    else if (!Score ) {
+        printf("please enter number only.");
+    }
+    else  {
+        printf("F !");
+    }
 
     return 0 ;
 }//end main function
